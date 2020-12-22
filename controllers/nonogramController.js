2e025f742c0bde6adb7edb235a4740e5ff7c69b6
ZoomIsland <zacherymilan@gmail.com>
2020-12-22 15:19:44 -0700

    Update and Delete routes work (no protection yet)

--- controllers/nonogramController.js
+++ controllers/nonogramController.js
@@ -11,23 +11,48 @@ router.get('/', (req, res) => {
 })
 
 // Show route
-router.get('/:id', (req, res) => {
+router.get('/:id/', (req, res) => {
   db.Nonogram.findById((req.params.id), (err, foundNonogram) => {
     if (err) return console.log(err);
     res.send(foundNonogram)
   });
 });
 
+// Create route
 router.post('/', (req, res) => {
   console.log(req.body);
   // does anything need to happen prior to post?
-  // maybe add Approval === false?
-  // though I'm hoping to make that default
   db.Nonogram.create(req.body, (err, newNonogram) => {
     if (err) return console.log(err);
     console.log(newNonogram);
     res.send(newNonogram);
     // will likely want a redirect instead
+    // back to home? Or to the page itself?
+    // res.redirect('/:id/');
+  })
+})
+
+// Update route (should only be for the user or admin)
+router.put('/:id/', (req, res) => {
+  db.Nonogram.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true},
+    (err, updatedNonogram) => {
+      if (err) return console.log(err);
+      res.send(updatedNonogram)
+      // probably should redirect to the page itself
+      // res.redirect('/:id/');
+    }
+  )
+})
+
+// Destroy route (only for Admin)
+router.delete('/:id/', (req, res) => {
+  db.Nonogram.findByIdAndDelete(req.params.id, (err, deletedNonogram) => {
+    if (err) return console.log(err);
+    res.send(deletedNonogram);
+    // should likely redirect somewhere. Home?
     // res.redirect('/');
   })
 })
